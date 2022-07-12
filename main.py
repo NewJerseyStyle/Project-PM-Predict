56d1f895d0d183956c5c5cef8e90da952cfdcf37
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2022-07-13 01:32:20 +0800

    add args choice of sentiment analysis engine

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import argparse
 from crawler import run as download_data
 from model import run as analysis_data
 from magi import run as top_five_predict
 from model import Engine
 
-def main():
+def main(args):
     download_data()
-    analysis_data()
+    if args.engine.upper() == 'NLTK':
+        analysis_data(Engine.NLTK)
+    elif args.engine.upper() == 'HUGGINGFACE':
+        analysis_data(Engine.HUGGINGFACE)
+    else:
+        raise NotImplementedError
     top5 = top_five_predict()
     for p in top5:
         print(p[0])
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='#MAGI_SYS challenge UK Politics')
+    parser.add_argument('--engine', type=str,
+                        default='NLTK', choices=['HUGGINGFACE', 'NLTK'],
+                        help='Engine to be used in sentiment analysis')
+
+    main(parser.parse_args())
