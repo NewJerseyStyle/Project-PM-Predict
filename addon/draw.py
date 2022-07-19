b9441cd0109864bf9974dbb619a640d080ca3f65
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2022-07-19 17:31:37 +0800

    bugfix DB reading & Html rendering

--- addon/draw.py
+++ addon/draw.py
@@ -1,29 +1,22 @@
-from multiprocessing import Process
-from flask import Flask, render_template
+from html2image import Html2Image
 from pyppeteer import launch
+from tinydb import TinyDB
 import asyncio
 
+from .css import get_css
+from .html import get_html
+
 async def draw():
+  hti = Html2Image()
+  db = TinyDB('db.json')
   rs_db = db.table('ranks')
   for ds in rs_db:
-    app = Flask(__name__)
-    @app.route('/')
-    def html():
-      return render_template('index.html',
-                             name=ds['name'],
-                             data_melchior=ds['a'],
-                             data_balthasar=ds['b'],
-                             data_casper=(ds['a'] + ds['b']) / 2)
-    server = Process(target=app.run) #app.run(host='127.0.0.1', port=8000)
-    server.start()
-    #screeen cap
-    browser = await launch(headless=True)
-    page = await browser.newPage()
-    await page.goto('http://127.0.0.1:8000/')
-    await page.screenshot({'path': f'{ds['name']}.png'})
-    await browser.close()
-    server.terminate()
-    server.join()
+    css = get_css()
+    html = get_html(name=ds['name'],
+                    data_melchior=ds['a'],
+                    data_balthasar=ds['b'],
+                    data_casper=(ds['a'] + ds['b']) / 2)
+    hti.screenshot(html_str=html, css_str=css, save_as=f'{ds["name"]}.png')
 
 
 def main():
